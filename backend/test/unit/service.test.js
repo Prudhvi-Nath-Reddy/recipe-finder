e60b3abe3c29b6468aa37f79aa87156db147cebf
Prudhvi-Nath-Reddy <mymail.prudhvi@gmail.com>
2023-12-15 12:42:35 +0530

    tests

--- backend/test/unit/service.test.js
+++ backend/test/unit/service.test.js
@@ -6,16 +6,15 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Recipe API Tests', () => {
-    // Assuming some recipes are already stored in the database for testing
+
 
     it('should get all recipes', async () => {
         try {
             const res = await chai.request(app)
-                .post('/getrecipe');  // Use the correct HTTP method and endpoint
+                .post('/getrecipe');  
 
             expect(res).to.have.status(200);
             expect(res.body).to.be.an('array').that.is.not.empty;
-            // You can add more specific assertions based on your expected response structure
         } catch (err) {
             console.error('Error during getRecipes test:', err);
             throw err;
